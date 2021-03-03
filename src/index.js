dadd9f2acc48de4204d46628eee2dc7046e666df
Alexson <alexsoncoelho21@hotmail.com>
2021-03-03 11:53:46 -0500

    reordering tasks

--- src/index.js
+++ src/index.js
@@ -9,11 +9,43 @@ const App = () => {
   const [data, setData] = useState(initialData);
 
   const onDragEnd = result => {
-    //Todo:reorder our column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    // re-ordering tasks
+    const column = data.columns[source.droppableId];
+    const newTaskIds = [...column.taskIds];
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    // updating state with new order
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    setData(newState);
   };
 
   return (
-    <DragDropContext onDragEnd={() => onDragEnd}>
+    <DragDropContext onDragEnd={onDragEnd}>
       {data.columnOrder.map(columnId => {
         const column = data.columns[columnId];
         const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
